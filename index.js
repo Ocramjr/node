9cf6988a4a3e6df1181433e9af7a1caf8503b592
MARCO ANTONIO PINTO MESQUITA JUNIOR <MARCO0216@DIGITALCOLLEGE.LOCAL>
2023-11-23 19:51:01 -0300

    Utilizando o método POST no Node

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const express = require('express')
+import express from "express"
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 const users = [
     {
@@ -51,35 +53,41 @@ app.listen(port, () => {
  * Um endpoint que mostre todos os produtos;
  * Um endpoint que mostre um produto específico;
  * Um endpoint que mostre o valor total do estoque por produto;
+ * Um endpoint que cadastre um novo produto
  */
 
 
 const produtos = [
     {
+        id: "1",
         nome:"computador",
         quantidade: 10,
         valorUnit: 2000,
 
     },
     {
+        id: "2",
         nome:"disco-rigido",
         quantidade: 20,
         valorUnit: 100,
 
     },
     {
+        id: "3",
         nome:"teclado",
         quantidade: 15,
         valorUnit: 150,
 
     },
     {
+        id: "4",
         nome:"mouse",
         quantidade: 20,
         valorUnit: 100,
 
     },
     {
+        id: "5",
         nome:"gabinete",
         quantidade: 10,
         valorUnit: 200,
@@ -93,29 +101,95 @@ app.get ('/produtos', (req, res) => {
 
 app.get ('/produtos/valortotal', (req, res) => {
     const valorTotal = produtos.map((produto) => {
-        return `${produto.nome} - Estoque em R$ ${produto.quantidade * produto.valorUnit},00` 
+        return {
+            [produto.nome]: produto.quantidade * produto.valorUnit,
+        } 
     })
     res.send(valorTotal)
 })
 
 
-app.get ('/produtos/:nome', (req, res) => {
-    const espProduto = produtos.find((produto) => produto.nome === req.params.nome)
+app.get ('/produto/:id', (req, res) => {
+    const espProduto = produtos.find((produto) => produto.id === req.params.id)
     if(espProduto) {
         res.send(espProduto)
     } else {
-        res.send(`Produto ${req.params.nome} não foi encontrado`)
+        res.send(`Produto ${req.params.id} não foi encontrado`)
     }
 })
 
+app.post("/produto", (req, res) => {
+    produtos.push(req.body)
+    res.json(produtos)
+})
 
 
+/**
+ * Array de objetos
+ * -> Lojas
+ * {
+ *  id: "1"
+ *  nome: "Nome da Loja",
+ *  endereco: "Av. Luz",
+ *  faturamento: 150,
+ * }
+ * 
+ * 
+ * Um endpoint que mostre todos as lojas;
+ * Um endpoint que mostre uma loja específica;
+ * Um endpoint que mostre o valor total do faturamento das lojas;
+ * Um endpoint que cadastre uma nova loja
+ */
 
-// const todosTrodutos = produtos.map((produto) => {
-//     return produto;
-// })
-// if (todosTrodutos){
-//     req.send(todosTrodutos)
-// } else {
-//     res.send('Não foi encontrado produtos')
-// }
+const lojas = [
+    {
+        id: "1",
+        nome: "North Shopping",
+        endereco: "Av. Bezerra de Menezes",
+        faturamento: 10000000,
+    },
+    {
+        id: "2",
+        nome: "Benfica",
+        endereco: "Av. Benfica",
+        faturamento: 15000000,
+    },
+    {
+        id: "3",
+        nome: "Aldeota",
+        endereco: "Av. Aldeota",
+        faturamento: 20000000,
+    },
+    {
+        id: "4",
+        nome: "Iguatemi",
+        endereco: "Av. Washigton Soares",
+        faturamento: 50000000,
+    },
+    
+]
+
+
+app.get("/lojas", (req, res) => {
+    res.json(lojas)
+})
+
+app.get("/loja/:id", (req, res) => {
+    const espLoja = lojas.find((loja) => loja.id === req.params.id)
+    res.json(espLoja)
+})
+
+app.get("/lojas/faturamento-lojas", (req, res) => {
+    const faturamentoLojas = []
+    for (const loja of lojas){
+        faturamentoLojas.push({
+            [loja.nome]: loja.faturamento,
+        })
+    }
+    res.json(faturamentoLojas)
+})
+
+app.post("/loja", (req, res) => {
+    lojas.push(req.body)
+    res.json(lojas)
+})
